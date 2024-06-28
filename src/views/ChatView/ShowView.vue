541fb6db00819430028d42ab0d6da6cfd27a5015
Marsh <3549684852@qq.com>
2024-06-28 19:36:07 +0800

    增加消息浮动显示效果前保存一下

--- src/views/ChatView/ShowView.vue
+++ src/views/ChatView/ShowView.vue
@@ -9,12 +9,14 @@ import {
   LoadingOutlined,
   DeleteOutlined,
   FormOutlined,
+  AudioOutlined,
+  AudioMutedOutlined,
 } from '@ant-design/icons-vue';
 import {h} from 'vue';
 import {message} from "ant-design-vue";
 
 const counter = useCounterStore()
-
+const microphone_status = ref(false);
 marked.setOptions({
   highlight: function (code, lang) {
     try {
@@ -111,7 +113,6 @@ const push_message = (event) => {
   if (event.key === 'Enter' && !event.shiftKey) {
     seed_message();
     event.preventDefault();
-    textarea_input.value = ''
   }
 }
 let temp_meg = ref("")
@@ -119,6 +120,7 @@ const chat_status_bool = ref(false)
 const seed_message = () => {
   const url1 = "/api/chat/add_con_data"
   const chat_con_user = textarea_input.value
+  textarea_input.value = ''
   let body = {
     chat_rec_id: chat_rec_id.value,
     textarea_input: chat_con_user
@@ -354,6 +356,12 @@ const click_copy = (text: string) => {
 const stop_chat = () => {
   chat_status_bool.value = false
 }
+const seed_microphone = () => {
+  microphone_status.value = true
+}
+const seed_microphone_stop = () => {
+  microphone_status.value = false
+}
 </script>
 <template>
 
@@ -498,36 +506,86 @@ const stop_chat = () => {
       <a-textarea
           v-model:value="textarea_input"
           placeholder="输入消息内容"
-          :autoSize="{ minRows: 2, maxRows: 12}"
-          style="width: 40%"
+          :autoSize="{ minRows: 5, maxRows: 5}"
+          style="width: 100%"
           :allowClear="true"
           class="textarea-input"
+          :bordered="false"
           @keydown="push_message"
       />
-      <icon :style="{ color: '#000000'}" class="icon-div4-seed" @click="stop_chat" v-if="!chat_status_bool">
-        <template #component>
-          <svg t="1719487236129" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-               p-id="13290" width="36" height="36">
-            <path
-                d="M512.877641 511.997001m-301.216175 301.216175a425.984 425.984 0 1 0 602.43235-602.43235 425.984 425.984 0 1 0-602.43235 602.43235Z"
-                fill="#000000" p-id="13291" data-spm-anchor-id="a313x.search_index.0.i17.4ed63a81Ka2GI0"
-                class="selected"></path>
-            <path
-                d="M266.24 427.7248l422.5024-103.5776a24.9856 24.9856 0 0 1 28.4672 34.9696l-188.2624 395.1616a24.9344 24.9344 0 0 1-46.08-3.072l-47.9744-149.4016a24.7808 24.7808 0 0 1 5.12-24.3712L522.24 486.4a6.6048 6.6048 0 0 0-8.192-10.24l-109.0048 63.6416a24.9856 24.9856 0 0 1-24.6272 0.3072L260.2496 473.8048a24.9856 24.9856 0 0 1 5.9904-46.08z"
-                fill="#FFFFFF" p-id="13292"></path>
-          </svg>
-        </template>
-      </icon>
-      <icon :style="{ color: '#000000'}" class="icon-div4-stop" @click="stop_chat" v-if="chat_status_bool">
-        <template #component>
-          <svg t="1719484882928" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-               p-id="2570" width="32" height="32">
-            <path
-                d="M512 42.666667C252.793333 42.666667 42.666667 252.793333 42.666667 512s210.126667 469.333333 469.333333 469.333333 469.333333-210.126667 469.333333-469.333333S771.206667 42.666667 512 42.666667z m213.333333 645.333333a37.373333 37.373333 0 0 1-37.333333 37.333333H336a37.373333 37.373333 0 0 1-37.333333-37.333333V336a37.373333 37.373333 0 0 1 37.333333-37.333333h352a37.373333 37.373333 0 0 1 37.333333 37.333333z"
-                fill="#000000" p-id="2571"></path>
-          </svg>
-        </template>
-      </icon>
+      <div class="div4-utils">
+        <div class="div4-microphone">
+          <AudioOutlined v-if="!microphone_status" class="div4-microphone1" @click="seed_microphone"/>
+          <icon :style="{ color: '#000000'}" v-if="microphone_status" class="div4-microphone2"
+                @click="seed_microphone_stop">
+            <template #component>
+              <svg t="1719568564008" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                   xmlns="http://www.w3.org/2000/svg"
+                   p-id="7474" width="25" height="25">
+                <path
+                    d="M128 128m58.0608 0l651.9296 0q58.0608 0 58.0608 58.0608l0 651.9296q0 58.0608-58.0608 58.0608l-651.9296 0q-58.0608 0-58.0608-58.0608l0-651.9296q0-58.0608 58.0608-58.0608Z"
+                    fill="#000000" p-id="7475"></path>
+              </svg>
+            </template>
+          </icon>
+        </div>
+        <icon :style="{ color: '#000000'}" class="div4-gallery">
+          <template #component>
+            <svg t="1719564468784" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                 p-id="3965" width="30" height="30">
+              <path
+                  d="M828.8 598.4c-6.4-6.4-12.8-9.6-22.4-9.6-9.6 0-16 3.2-22.4 9.6L694.4 688c-12.8 12.8-12.8 32 0 44.8 12.8 12.8 32 12.8 44.8 0l35.2-35.2V896c0 16 12.8 32 32 32s32-12.8 32-32V697.6l38.4 38.4c12.8 12.8 32 12.8 44.8 0 12.8-12.8 12.8-32 0-44.8l-92.8-92.8z m-419.2-240c16-35.2 6.4-76.8-19.2-105.6-28.8-28.8-73.6-35.2-108.8-22.4-35.2 16-57.6 54.4-57.6 92.8 0 51.2 41.6 92.8 92.8 92.8 38.4 0 76.8-22.4 92.8-57.6zM288 323.2c0-9.6 3.2-16 9.6-22.4 3.2-9.6 12.8-12.8 22.4-12.8 16 0 32 12.8 32 28.8s-12.8 32-28.8 35.2c-19.2 0-35.2-12.8-35.2-28.8z"
+                  fill="#000000" p-id="3966"></path>
+              <path
+                  d="M896 96H128c-35.2 0-64 28.8-64 64v640c0 35.2 28.8 64 64 64h544v-64H160c-19.2 0-32-16-32-32V192c0-16 12.8-32 32-32h704c19.2 0 32 16 32 32v416h64V160c0-35.2-28.8-64-64-64z"
+                  fill="#000000" p-id="3967"></path>
+              <path
+                  d="M182.4 713.6c12.8 12.8 32 12.8 44.8 0l134.4-134.4 112 112c3.2 3.2 3.2 3.2 6.4 3.2 12.8 6.4 25.6 6.4 38.4-3.2l316.8-316.8c12.8-12.8 12.8-32 0-44.8-12.8-12.8-32-12.8-44.8 0L496 624 384 508.8l-3.2-3.2c-12.8-9.6-28.8-9.6-41.6 3.2l-160 160c-9.6 12.8-9.6 32 3.2 44.8z"
+                  fill="#000000" p-id="3968"></path>
+            </svg>
+          </template>
+        </icon>
+        <icon :style="{ color: '#000000'}" class="div4-clear">
+          <template #component>
+            <svg t="1719565031756" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                 p-id="5248" width="25" height="25">
+              <path
+                  d="M783.36 331.52l162.56-161.92a71.68 71.68 0 0 0-101.12-101.12L682.24 230.4h-7.04A183.04 183.04 0 0 0 448 251.52a5.76 5.76 0 0 0 0 7.68l307.2 306.56a5.12 5.12 0 0 0 7.04 0 183.04 183.04 0 0 0 20.48-227.2s0.64-4.48 0.64-7.04zM228.48 115.84a64 64 0 0 0 17.92 42.88 64 64 0 0 0 43.52 17.92 59.52 59.52 0 0 0 42.88-17.92 64 64 0 0 0 0-86.4 59.52 59.52 0 0 0-42.88-17.92 64 64 0 0 0-43.52 17.92 64 64 0 0 0-17.92 43.52zM689.92 822.4a76.16 76.16 0 0 0 22.4 54.4 77.44 77.44 0 0 0 53.76 22.4 76.8 76.8 0 0 0 76.8-76.8A76.8 76.8 0 0 0 768 746.24a74.24 74.24 0 0 0-53.76 22.4 73.6 73.6 0 0 0-24.32 53.76zM908.8 754.56a41.6 41.6 0 1 0 12.16-29.44 42.24 42.24 0 0 0-12.16 29.44zM393.6 308.48l305.92 305.92a7.04 7.04 0 0 1 0 8.32A905.6 905.6 0 0 0 576 974.72l-104.32-64C480 890.24 576 694.4 564.48 704a1314.56 1314.56 0 0 0-123.52 149.12l-28.16 38.4a792.32 792.32 0 0 1-142.72-119.68c21.76-26.88 137.6-177.28 137.6-177.28L211.2 710.4a998.4 998.4 0 0 1-96.64-117.12l128-104.96-149.12 52.48-60.8-106.24A1183.36 1183.36 0 0 0 384 307.2a6.4 6.4 0 0 1 9.6 1.28z"
+                  fill="#000000" p-id="5249"></path>
+            </svg>
+          </template>
+        </icon>
+        <span class="div4-seed-desc">Enter发送 / Shift+Enter换行</span>
+        <div class="div4-seed" @click="seed_message">
+          <icon :style="{ color: '#000000'}" class="icon-div4-seed" @click="stop_chat" v-if="!chat_status_bool">
+            <template #component>
+              <svg t="1719487236129" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                   xmlns="http://www.w3.org/2000/svg"
+                   p-id="13290" width="32" height="32">
+                <path
+                    d="M512.877641 511.997001m-301.216175 301.216175a425.984 425.984 0 1 0 602.43235-602.43235 425.984 425.984 0 1 0-602.43235 602.43235Z"
+                    fill="#000000" p-id="13291" data-spm-anchor-id="a313x.search_index.0.i17.4ed63a81Ka2GI0"
+                    class="selected"></path>
+                <path
+                    d="M266.24 427.7248l422.5024-103.5776a24.9856 24.9856 0 0 1 28.4672 34.9696l-188.2624 395.1616a24.9344 24.9344 0 0 1-46.08-3.072l-47.9744-149.4016a24.7808 24.7808 0 0 1 5.12-24.3712L522.24 486.4a6.6048 6.6048 0 0 0-8.192-10.24l-109.0048 63.6416a24.9856 24.9856 0 0 1-24.6272 0.3072L260.2496 473.8048a24.9856 24.9856 0 0 1 5.9904-46.08z"
+                    fill="#FFFFFF" p-id="13292"></path>
+              </svg>
+            </template>
+          </icon>
+          <icon :style="{ color: '#000000'}" class="icon-div4-stop" @click="stop_chat" v-if="chat_status_bool">
+            <template #component>
+              <svg t="1719484882928" class="icon" viewBox="0 0 1024 1024" version="1.1"
+                   xmlns="http://www.w3.org/2000/svg"
+                   p-id="2570" width="32" height="32">
+                <path
+                    d="M512 42.666667C252.793333 42.666667 42.666667 252.793333 42.666667 512s210.126667 469.333333 469.333333 469.333333 469.333333-210.126667 469.333333-469.333333S771.206667 42.666667 512 42.666667z m213.333333 645.333333a37.373333 37.373333 0 0 1-37.333333 37.333333H336a37.373333 37.373333 0 0 1-37.333333-37.333333V336a37.373333 37.373333 0 0 1 37.333333-37.333333h352a37.373333 37.373333 0 0 1 37.333333 37.333333z"
+                    fill="#000000" p-id="2571"></path>
+              </svg>
+            </template>
+          </icon>
+          <span style="cursor: pointer">发送</span>
+        </div>
+      </div>
     </div>
 
 
@@ -542,42 +600,122 @@ const stop_chat = () => {
 
 .div4 {
   width: 99.96%;
-  height: 20vh;
-  background: @theme-background-color2;
+  height: 19.65vh;
+  display: flex;
+  flex-direction: column;
 
-  .textarea-input {
-    position: absolute; /* 使用绝对定位 */
-    bottom: 0; /* 固定在底部 */
-    margin-bottom: 3vh;
-    left: 50%; /* 将左侧位置设置为父元素的50% */
-    transform: translate(-50%, 0); /* 向左移动自身宽度的50%，以达到水平居中 */
+  .div4-utils {
+    display: flex;
+
+
+    .textarea-input {
+      position: relative;
+    }
+
+    .div4-microphone {
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      left: 0.5vw;
+      top: 0.8vh;
+      width: 2vw;
+
+      .div4-microphone1 {
+        font-size: 24px;
+        transition: color 0.3s ease;
+
+        &:hover {
+          color: #2664c5;
+        }
+      }
+
+      .div4-microphone2 {
+        font-size: 24px;
+        transition: color 0.3s ease;
+
+        &:hover {
+          color: #2664c5;
+        }
+      }
+    }
+
+    .div4-gallery {
+      position: relative;
+      left: 1vw;
+      top: 1.1vh;
+
+      .icon:hover path {
+        fill: #2664c5;
+      }
+    }
+
+    .div4-gallery .icon path {
+      transition: fill 0.3s ease;
+    }
+
+    .div4-clear {
+      position: relative;
+      top: 1.5vh;
+      left: 57.5vw;
+
+      .icon:hover path {
+        fill: #2664c5;
+      }
+    }
+
+    .div4-clear .icon path {
+      transition: fill 0.3s ease;
+    }
+
+    .div4-seed-desc {
+      position: relative;
+      left: 58.5vw;
+      top: 1.7vh;
+      color: #b4b4b4;
+    }
+
+    .div4-seed {
+      display: flex;
+      align-items: center;
+      position: relative;
+      left: 59vw;
+      top: 1vh;
+      padding: 0.01vh 1.5vh 0.01vh 1.5vh;
+      border: 1px solid #b0b0b3;
+      transition: color 0.3s ease, border-color 0.3s ease;
+      border-radius: 5px;
+
+      &:hover {
+        border: 1px solid #3085fb;
+      }
+
+      .icon-div4-seed {
+        position: relative;
+
+      }
+
+      .icon-div4-stop {
+        position: relative;
+      }
+    }
+
+
   }
-
-  .icon-div4-seed {
-    position: relative;
-    left: 56vw;
-    top: 2vh;
-  }
-
-  .icon-div4-stop {
-    position: relative;
-    left: 56vw;
-    top: 1.8vh;
-  }
-
 
 }
 
 .div3 {
   width: 99.96%;
-  height: 80vh;
+  height: 70vh;
   background-color: @theme-background-color;
+  border-bottom: 1px solid @theme-border-color;
   overflow: auto;
 
   .div-div-user {
     display: flex;
     justify-content: flex-end;
-    margin-right: 20vw;
+    margin-right: 2vw;
 
     .ant-head2-user {
       background-color: #e8e4e4;
@@ -605,7 +743,7 @@ const stop_chat = () => {
   .div-div-user2 {
     display: flex;
     justify-content: flex-end;
-    margin-right: 20vw;
+    margin-right: 2vw;
 
     .div-content-user {
       margin-top: 1%;
@@ -614,11 +752,9 @@ const stop_chat = () => {
       min-height: 10%; /* 设置最小高度 */
       background: #dfe8fd;
       border-radius: 5px 0 5px 5px;
+      padding: 0.01vh 1vh 0.01vh 1vh;
       word-wrap: break-word; /* 设置文字换行 */
       overflow-wrap: break-word; /* 设置文字换行 */
-
-      .div-content-txt-user {
-      }
     }
 
   }
@@ -627,7 +763,7 @@ const stop_chat = () => {
   .div-div-ai {
     display: flex;
     justify-content: flex-start;
-    margin-left: 12vw;
+    margin-left: 2vw;
 
     .ant-head2-ai {
       background-color: #e8e4e4;
@@ -651,7 +787,7 @@ const stop_chat = () => {
   .div-div-ai2 {
     display: flex;
     justify-content: flex-start;
-    margin-left: 12vw;
+    margin-left: 2vw;
 
     .div-content-ai {
       margin-top: 1%;
@@ -660,6 +796,7 @@ const stop_chat = () => {
       min-height: 10%; // 设置最小高度
       background: #f5f6f8;
       border-radius: 0 5px 5px 5px;
+      padding: 0.01vh 1vh 0.01vh 1vh;
       word-wrap: break-word; // 设置文字换行
       overflow-wrap: break-word; // 设置文字换行
 
