6ea1694666835b60c3b1348bd86d068e3d2b67f6
Marsh <3549684852@qq.com>
2024-06-28 23:02:08 +0800

    语音功能完成了，删除单条对话，刷新重问，清楚上下文待完成

--- src/views/ChatView/ShowView.vue
+++ src/views/ChatView/ShowView.vue
@@ -5,17 +5,222 @@ import {marked} from 'marked';
 import hljs from 'highlight.js'
 import Icon from '@ant-design/icons-vue';
 import {onMounted} from "vue";
+
 import {
   LoadingOutlined,
   DeleteOutlined,
   FormOutlined,
   AudioOutlined,
-  AudioMutedOutlined,
 } from '@ant-design/icons-vue';
 import {h} from 'vue';
 import {message} from "ant-design-vue";
+import '/src/voice-utils/utilJS/crypto-js.js'; //鉴权的引用地址
+import '/src/voice-utils/utilJS/index.umd.js'; // 调用Web Speech API 的依赖，应该是官方的写的工具类
+const btnText = ref("开始录音");
+const btnStatus = ref("UNDEFINED"); // "UNDEFINED" "CONNECTING" "OPEN" "CLOSING" "CLOSED"
+const recorder = new RecorderManager('/src/voice-utils/dist')
+const APPID = "20c0ca0e"; // TODO 你的讯飞模型APPID
+const API_SECRET = "ODkwZWUzZjc3NmNmMTc0ZTA3ZTI2Y2I3"; // TODO 你的讯飞模型API_SECRET
+const API_KEY = "0966746747f331913453687203fad7ae"; // TODO 你的讯飞模型API_KEY
+let iatWS; //监听录音的变量
+let resultTextTemp = ref('');
+let countdownInterval;
+
+// 生成 WebSocket URL 生成规则由平台决定
+function getWebSocketUrl() {
+  // 请求地址根据语种不同变化
+  var url = "wss://iat-api.xfyun.cn/v2/iat";
+  var host = "iat-api.xfyun.cn";
+  var apiKey = API_KEY;
+  var apiSecret = API_SECRET;
+  var date = new Date().toGMTString();
+  var algorithm = "hmac-sha256";
+  var headers = "host date request-line";
+  var signatureOrigin = `host: ${host}\ndate: ${date}\nGET /v2/iat HTTP/1.1`;
+  var signatureSha = CryptoJS.HmacSHA256(signatureOrigin, apiSecret);
+  var signature = CryptoJS.enc.Base64.stringify(signatureSha);
+  var authorizationOrigin = `api_key="${apiKey}", algorithm="${algorithm}", headers="${headers}", signature="${signature}"`;
+  var authorization = btoa(authorizationOrigin);
+  url = `${url}?authorization=${authorization}&date=${date}&host=${host}`;
+  return url;
+}
+
+// 加密工具函数
+function toBase64(buffer) {
+  var binary = "";
+  var bytes = new Uint8Array(buffer);
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
+}
+
+// 计数函数
+function countdown() {
+  let seconds = 60;
+  btnText.value = `录制中：${seconds}s`;
+  countdownInterval = setInterval(() => {
+    seconds = seconds - 1;
+    if (seconds <= 0) {
+      clearInterval(countdownInterval);
+      recorder.stop();
+    } else {
+      btnText.value = `录制中：${seconds}s`;
+    }
+  }, 1000);
+}
+
+// 录音状态变化函数
+function changeStatus(status) {
+  btnStatus.value = status;
+  if (status === "CONNECTING") {
+    btnText.value = "建立连接中";
+    resultTextTemp.value = "";
+  } else if (status === "OPEN") {
+    countdown();
+  } else if (status === "CLOSING") {
+    btnText.value = "关闭连接中";
+  } else if (status === "CLOSED") {
+    btnText.value = "开始录制";
+  }
+}
+
+// 结果解析函数
+function renderResult(resultData) {
+  // 识别结束
+  let jsonData = JSON.parse(resultData);
+  if (jsonData.data && jsonData.data.result) {
+    let data = jsonData.data.result;
+    let str = "";
+    let ws = data.ws;
+    for (let i = 0; i < ws.length; i++) {
+      str = str + ws[i].cw[0].w;
+    }
+    // 开启wpgs会有此字段(前提：在控制台开通动态修正功能)
+    // 取值为 "apd"时表示该片结果是追加到前面的最终结果；取值为"rpl" 时表示替换前面的部分结果，替换范围为rg字段
+    if (data.pgs) {
+      if (data.pgs === "apd") {
+        // 将resultTextTemp同步给
+        textarea_input.value = resultTextTemp;
+      }
+      // 将结果存储在resultTextTemp中
+      resultTextTemp.value = textarea_input.value + str;
+    } else {
+      textarea_input.value = textarea_input.value + str;
+    }
+  }
+  if (jsonData.code === 0 && jsonData.data.status === 2) {
+    iatWS.close();
+  }
+  if (jsonData.code !== 0) {
+    iatWS.close();
+    console.error(jsonData);
+  }
+}
+
+// 连接 WebSocket
+function connectWebSocket() {
+  const websocketUrl = getWebSocketUrl();
+  if ("WebSocket" in window) {
+    iatWS = new WebSocket(websocketUrl);
+  } else if ("MozWebSocket" in window) {
+    iatWS = new MozWebSocket(websocketUrl);
+  } else {
+    alert("浏览器不支持WebSocket");
+    return;
+  }
+  changeStatus("CONNECTING");
+  iatWS.onopen = (e) => {
+    // 开始录音
+    recorder.start({
+      sampleRate: 16000,
+      frameSize: 1280,
+    });
+    var params = {
+      common: {
+        app_id: APPID,
+      },
+      business: {
+        language: "zh_cn",
+        domain: "iat",
+        accent: "mandarin",
+        vad_eos: 5000,
+        dwa: "wpgs",
+      },
+      data: {
+        status: 0,
+        format: "audio/L16;rate=16000",
+        encoding: "raw",
+      },
+    };
+    iatWS.send(JSON.stringify(params));
+  };
+  iatWS.onmessage = (e) => {
+    renderResult(e.data);
+  };
+  iatWS.onerror = (e) => {
+    console.error(e);
+    recorder.stop();
+    changeStatus("CLOSED");
+  };
+  iatWS.onclose = (e) => {
+    recorder.stop();
+    changeStatus("CLOSED");
+  };
+}
+
+// 定义监听开始的函数
+recorder.onStart = () => {
+  changeStatus("OPEN");
+}
+// 处理回调的结果
+recorder.onFrameRecorded = ({isLastFrame, frameBuffer}) => {
+  if (iatWS.readyState === iatWS.OPEN) {
+    iatWS.send(
+        JSON.stringify({
+          data: {
+            status: isLastFrame ? 2 : 1,
+            format: "audio/L16;rate=16000",
+            encoding: "raw",
+            audio: toBase64(frameBuffer),
+          },
+        })
+    );
+    if (isLastFrame) {
+      changeStatus("CLOSING");
+    }
+  }
+};
+// 停止录音的处理
+recorder.onStop = () => {
+  clearInterval(countdownInterval);
+};
+// 按钮点击的启动 | 结束函数
+const startRecording = () => {
+  if (btnStatus.value === "UNDEFINED" || btnStatus.value === "CLOSED") {
+    microphone_status.value = true
+    connectWebSocket();
+  } else if (btnStatus.value === "CONNECTING" || btnStatus.value === "OPEN") {
+    // 结束录音
+    recorder.stop();
+    microphone_status.value = false
+  }
+}
+
 
 const counter = useCounterStore()
+
+
+const seed_microphone = () => {
+  microphone_status.value = true
+
+}
+const seed_microphone_stop = () => {
+  microphone_status.value = false
+}
+
+
 const microphone_status = ref(false);
 marked.setOptions({
   highlight: function (code, lang) {
@@ -118,6 +323,10 @@ const push_message = (event) => {
 let temp_meg = ref("")
 const chat_status_bool = ref(false)
 const seed_message = () => {
+  if (textarea_input.value.length == 0) {
+    message.warn("输入不能为空")
+    return
+  }
   const url1 = "/api/chat/add_con_data"
   const chat_con_user = textarea_input.value
   textarea_input.value = ''
@@ -356,12 +565,29 @@ const click_copy = (text: string) => {
 const stop_chat = () => {
   chat_status_bool.value = false
 }
-const seed_microphone = () => {
-  microphone_status.value = true
+
+const delete_message = (id) => {
+  counter.contents = counter.contents.filter(item => item[0] !== id)
+  let body = {
+    chat_con_id: id,
+  }
+  const url = "/api/chat/delete/chat_con_id"
+  fetch(url, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(body),
+    credentials: "include"
+  }).then((res) => {
+    if (res.ok) {
+      return res.json()
+    }
+  }).then((data) => {
+    if (data["code"] == 1) {
+      message.success("删除成功")
+    }
+  })
 }
-const seed_microphone_stop = () => {
-  microphone_status.value = false
-}
+
 </script>
 <template>
 
@@ -426,7 +652,7 @@ const seed_microphone_stop = () => {
                 </template>
               </icon>
             </a-radio-button>
-            <a-radio-button value="c">
+            <a-radio-button value="c" @click="delete_message(item[0])">
               <DeleteOutlined class="icon-user-red" :style="{ color: '#8994a6'}"/>
             </a-radio-button>
           </a-radio-group>
@@ -515,9 +741,9 @@ const seed_microphone_stop = () => {
       />
       <div class="div4-utils">
         <div class="div4-microphone">
-          <AudioOutlined v-if="!microphone_status" class="div4-microphone1" @click="seed_microphone"/>
-          <icon :style="{ color: '#000000'}" v-if="microphone_status" class="div4-microphone2"
-                @click="seed_microphone_stop">
+          <AudioOutlined v-if="!microphone_status" class="div4-microphone1" @click="startRecording()"/>
+          <icon :style="{ color: '#000000'}" v-show="microphone_status" class="div4-microphone2"
+                @click="startRecording()">
             <template #component>
               <svg t="1719568564008" class="icon" viewBox="0 0 1024 1024" version="1.1"
                    xmlns="http://www.w3.org/2000/svg"
@@ -528,6 +754,9 @@ const seed_microphone_stop = () => {
               </svg>
             </template>
           </icon>
+          <div class="div4-microphone-desc" v-show="microphone_status">
+            <span style="color: white;font-size: 12px">{{ btnText }}</span>
+          </div>
         </div>
         <icon :style="{ color: '#000000'}" class="div4-gallery">
           <template #component>
@@ -612,42 +841,68 @@ const seed_microphone_stop = () => {
       position: relative;
     }
 
+    .div4-microphone-desc {
+      position: absolute;
+      top: -4.5vh;
+      left: -0.2vw;
+      width: 5vw;
+      height: 4vh;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      border-radius: 5px;
+      justify-content: center;
+      background: #000000;
+    }
+
     .div4-microphone {
       position: relative;
       display: flex;
       align-items: center;
       justify-content: center;
       left: 0.5vw;
-      top: 0.8vh;
+      top: 1vh;
       width: 2vw;
 
       .div4-microphone1 {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
         font-size: 24px;
-        transition: color 0.3s ease;
+        padding: 0.77vh 1.3vh 0.77vh 1.3vh;
+        transition: background-color 0.3s ease, color 0.3s ease;
+        border-radius: 5px;
 
         &:hover {
+          background-color: #d0e4f4;
           color: #2664c5;
         }
       }
 
       .div4-microphone2 {
         font-size: 24px;
-        transition: color 0.3s ease;
-
-        &:hover {
-          color: #2664c5;
-        }
       }
     }
 
     .div4-gallery {
+      display: flex;
+      align-items: center;
+      justify-content: center;
       position: relative;
       left: 1vw;
-      top: 1.1vh;
+      top: 1vh;
+      padding: 0.3vh 1vh 0.3vh 1vh;
+      transition: background-color 0.3s ease;
+      border-radius: 5px;
+    }
 
-      .icon:hover path {
-        fill: #2664c5;
-      }
+    .div4-gallery:hover {
+      background-color: #d0e4f4;
+    }
+
+    .div4-gallery:hover .icon path {
+      fill: #2664c5;
     }
 
     .div4-gallery .icon path {
@@ -655,13 +910,25 @@ const seed_microphone_stop = () => {
     }
 
     .div4-clear {
+      display: flex;
+      align-items: center;
+      justify-content: center;
       position: relative;
-      top: 1.5vh;
-      left: 57.5vw;
+      top: 1vh;
+      left: 55.5vw;
+      padding: 0.3vh 1.2vh 0.3vh 1.2vh;
+      transition: background-color 0.3s ease;
+      border-radius: 5px;
 
-      .icon:hover path {
-        fill: #2664c5;
-      }
+
+    }
+
+    .div4-clear:hover {
+      background-color: #d0e4f4; /* 鼠标悬停时 div 的背景色 */
+    }
+
+    .div4-clear:hover .icon path {
+      fill: #2664c5; /* 鼠标悬停时 icon 的填充色 */
     }
 
     .div4-clear .icon path {
@@ -670,7 +937,7 @@ const seed_microphone_stop = () => {
 
     .div4-seed-desc {
       position: relative;
-      left: 58.5vw;
+      left: 56.5vw;
       top: 1.7vh;
       color: #b4b4b4;
     }
@@ -678,16 +945,16 @@ const seed_microphone_stop = () => {
     .div4-seed {
       display: flex;
       align-items: center;
+      justify-content: center;
       position: relative;
-      left: 59vw;
+      left: 57vw;
       top: 1vh;
-      padding: 0.01vh 1.5vh 0.01vh 1.5vh;
-      border: 1px solid #b0b0b3;
-      transition: color 0.3s ease, border-color 0.3s ease;
+      padding: 0.3vh 1.5vh 0.3vh 1.5vh;
+      transition: background-color 0.3s ease;
       border-radius: 5px;
 
       &:hover {
-        border: 1px solid #3085fb;
+        background-color: #d0e4f4;
       }
 
       .icon-div4-seed {
@@ -746,9 +1013,9 @@ const seed_microphone_stop = () => {
     margin-right: 2vw;
 
     .div-content-user {
-      margin-top: 1%;
-      margin-bottom: 1%;
-      max-width: 45vw;
+      margin-top: 0.5%;
+      margin-bottom: 2%;
+      max-width: 60vw;
       min-height: 10%; /* 设置最小高度 */
       background: #dfe8fd;
       border-radius: 5px 0 5px 5px;
@@ -790,9 +1057,9 @@ const seed_microphone_stop = () => {
     margin-left: 2vw;
 
     .div-content-ai {
-      margin-top: 1%;
+      margin-top: 0.5%;
       margin-bottom: 1%;
-      max-width: 45vw; // 指定最大宽度，超过这个宽度内容会自动换行
+      max-width: 60vw; // 指定最大宽度，超过这个宽度内容会自动换行
       min-height: 10%; // 设置最小高度
       background: #f5f6f8;
       border-radius: 0 5px 5px 5px;
--- src/views/WorkflowView/LayoutView.vue
+++ src/views/WorkflowView/LayoutView.vue
@@ -1,54 +1,11 @@
 <script setup lang="ts">
-import {useCounterStore} from '@/stores/counter'
-import {ref} from "vue";
 
-const counter = useCounterStore()
-localStorage.setItem('selectedKey', "2");
-const textarea_input = ref<string>('');
-counter.selectedKeys = [localStorage.getItem("selectedKey")]
 </script>
 
 <template>
-  <div class="div1">
-
-  </div>
-  <div class="div4">
-    <a-textarea
-        v-model:value="textarea_input"
-        placeholder="输入消息内容"
-        :autoSize="{ minRows: 2, maxRows: 12}"
-        style="width: 30vw"
-        :allowClear="true"
-        class="textarea-input"
-    />
-  </div>
+  工作流页面
 </template>
 
 <style scoped lang="less">
-@import "src/assets/css/theme.less";
 
-.div1 {
-  position: relative;
-  left: 25vw;
-  width: 40vw;
-  height: 10vh;
-  background: #1f6feb;
-}
-
-.div4 {
-  position: relative;
-  left: 25vw;
-  top: 70vh;
-  width: 40vw;
-  height: 10vh;
-  background: #1f6feb;
-
-  .textarea-input {
-    position: relative; /* 使用绝对定位 */
-    bottom: 0; /* 固定在底部 */
-    margin-bottom: 3vh;
-    left: 50%; /* 将左侧位置设置为父元素的50% */
-    transform: translate(-50%, 0); /* 向左移动自身宽度的50%，以达到水平居中 */
-  }
-}
 </style>
